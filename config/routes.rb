00cbab0c8f394d29ca99c8a7c1a8599966c058aa
Raghav Bhardwaj <allindiahacker@gmail.com>
2018-03-27 11:17:45 +0000

    Added Basic AlphaBlog UI

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   get 'about' , to: 'pages#about' 
-  root to: 'pages#home' 
+  root 'pages#home' 
+  
+  resources :articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
