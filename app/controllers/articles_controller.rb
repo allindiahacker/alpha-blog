d5ae158f9bc44ee01579837a54745e4eb8ce9924
Raghav Bhardwaj <allindiahacker@gmail.com>
2018-03-29 18:00:59 +0000

    Article repo listing links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,14 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+  
   def new
     @article = Article.new 
   end
   
   def create
-    #render plain: params[:article].inspect  
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article yes"
@@ -17,6 +21,21 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article yes updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  
   
   private
   def article_params
